f6ba5068b0b502cfc7c6048d1f457681a3502f74
Afsana Bhuiyan <abhuiyan8983@bths.edu>
2024-01-29 16:47:03 -0500

    got top 20 movies calculated

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ def get_top_rated_movies(file_path, top_n=20):
     movies_df = movies_df[movies_df['vote_count'] > 50]
 
     # Sort by vote_average and select the top_n movies
-    top_movies = movies_df[['original_title', 'vote_average']].sort_values(by='vote_average', ascending=False).head(top_n)
+    top_movies_df = movies_df[['original_title', 'vote_average']].sort_values(by='vote_average', ascending=False).head(top_n)
 
-    return top_movies
+    # Convert the DataFrame to a list of dictionaries
+    top_movies_list = top_movies_df.to_dict('records')
+
+    return top_movies_list
 
 @app.route('/')
 def index():
-    # Use the consistent file path
     top_movies = get_top_rated_movies('data/movies_metadata.csv')
     return render_template('index.html', top_movies=top_movies)
 
